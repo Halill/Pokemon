ceff4a8afde2834443421a6497e0a8911ef98eb4
Halill <halil.oezdogan@hotmail.de>
2017-05-01 12:50:45 +0200

    i

--- public/game.php
+++ public/game.php
@@ -7,12 +7,13 @@ include '../loggedin.php';
 <!DOCTYPE html>
 <html>
 <head>
+
     <link href="/css/style.css" rel="stylesheet">
   	<link rel="shortcut icon" type="image/x-icon" href="bilder/pokeball.ico" />
     <title>Poke-Game</title>
 </head>
 <body>
- <iframe src="PlayerMovement.html" align="middle" scrolling="no" width="560" height="428"></iframe>
+ <iframe src="PlayerMovement.html" align="middle" scrolling="no" width="80%" height="80%"></iframe>
 
 <form method="post" action="logout.php">
   <input value="Logout" type="submit" name="sent">
