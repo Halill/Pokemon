0e4c29e0f75fc5abf852ebc45af0efa9585067bb
IT-Networks <Markus.Giesbrecht@gmx.net>
2017-05-03 12:27:11 +0200

    new

--- public/app.js
+++ public/app.js
@@ -1,41 +1,36 @@
 var SimpleGame = (function () {
     function SimpleGame() {
-        this.game = new Phaser.Game(2000 , 2000, Phaser.CANVAS, 'content', { preload: this.preload, create: this.create, update: this.update, render: this.render });
+        this.game = new Phaser.Game(560 , 428, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update, render: this.render });
     }
     SimpleGame.prototype.preload = function () {
-        this.game.load.image('background', '/bilder/background.jpg');
         this.game.load.image('Map1', '/assets/misc/Map1/Viridian_City_Map.png');
         this.game.load.image('heightmap', 'assets/misc/Map1/Heightmap.png');
 		this.game.load.image('texBox', 'assets/misc/Chat/chatBox.png');
-		this.game.load.text('text');
-        this.game.load.spritesheet('button', 'assets/buttons/button_sprite_sheet.png', 100, 100);
+      //  this.game.load.spritesheet('button', 'assets/buttons/button_sprite_sheet.png', 100, 100);
         this.game.load.spritesheet('player', 'assets/misc/Player/Player_Sprite.png', 19, 27);
     };
     SimpleGame.prototype.create = function () {
         this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         this.game.scale.pageAlignHorizontally = true;
         this.game.scale.pageAlignVertically = true;
-        this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'background');
-        this.logo.anchor.setTo(0.5, 0.5);
-        this.logo.scale.setTo(0.2, 0.2);
-        this.game.add.tween(this.logo.scale).to({ x: 1, y: 1 }, 2000, Phaser.Easing.Circular.Out, true);
-        this.button = this.game.add.button(this.game.world.centerX - 95, 750, 'button', actionOnClick, this, 2, 1, 0);
-        this.button.onInputOver.add(over, this);
-        this.button.onInputOut.add(out, this);
-        this.button.onInputUp.add(up, this);
-        this.map1 = this.game.add.sprite(0, 0, 'Map1');
-        //this.map1.anchor.setTo(0.5, 0.5);
-        this.map1.visible = false;
+
+		
+        this.bmd = this.game.make.bitmapData(1120, 928);
+        this.bmd.draw('heightmap',0,0,this.game.width,this.game.height);
+        this.bmd.update();
+		
+		this.map1 = this.game.add.sprite(0, 0, 'Map1');
+
+		this.map1.width = this.game.width;
+		this.map1.height = this.game.height;
+		
         this.player = this.game.add.sprite(300,400, 'player');
         this.player.name = "undefined";
-        this.player.visible = false;
+
 		
-		this.texBox = this.game.add.sprite(501,1002, 'texBox');
+		this.texBox = this.game.add.sprite(this.world.centerX, this.world.centerY, 'texBox');
         this.texBox.name = "chat";
         this.texBox.visible = false;
-        //this.house = this.game.add.sprite(380,0, 'house');
-        //this.house.name = "undefined";
-        //this.house.visible = false;
 		
         //Reservieren der Pfeiltasten für das Spiel. Dadurch wird verhindert, dass die Scrollbars der Website nicht darauf reagieren.
         var upKey;
@@ -46,11 +41,8 @@ var SimpleGame = (function () {
         downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
         leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
         rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-        this.game.stage.backgroundColor = '#000000';
-        this.bmd = this.game.make.bitmapData(1120, 928);
-        this.bmd.draw('heightmap');
-        this.bmd.update();
-		
+       // this.game.stage.backgroundColor = '#000000';
+
 		
 		this.text = this.game.add.text(516,1015, "- You have clicked -\n0 times !", {
         font: "20px Arial",
@@ -167,13 +159,13 @@ function out() {
     console.log('button out');
 }
 function actionOnClick() {
-    this.logo.visible = false;
-    this.button.visible = false;
-    this.map1.visible = true;
-    this.player.visible = true;
+	
+  /*   this.logo.visible = false;
+    this.button.visible = false; */
+
     this.player.frame = 1;
     //this.house.visible = true;
-    this.game.stage.backgroundColor = '#2d2d2d';
+//    this.game.stage.backgroundColor = '#2d2d2d';
     //  This will check Sprite vs. Sprite collision
     this.player.name = 'player';
     //this.house.name = 'house';
