bfe04f85a65afe7338e47cd4b7f6c37fc15a40ea
IT-Networks <Markus.Giesbrecht@gmx.net>
2017-05-07 21:32:18 +0200

    Signed-off-by: IT-Networks <Markus.Giesbrecht@gmx.net>

---
+++ public/getEnemyStrenght.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+$q = intval($_GET['q']);
+
+
+echo "Pokemon stk " + $q;
+?>
---
+++ public/getPokemonStk.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+$q = intval($_GET['q']);
+
+if(isset($_SESSION['userid'])) {
+	if(isset($_POST['save'])){
+		
+  }
+}
+
+echo $q;
+?>
